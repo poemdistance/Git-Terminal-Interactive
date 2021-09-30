1d6a8512bbc5dabe054a32e9cd622eb9d6b57f09
poemdistance <poemdistance@gmail.com>
2021-09-30 23:07:07 +0800

    fix compile error

---
+++ choice.c
@@ -0,0 +1,360 @@
+#include <unistd.h>
+#include <menu.h>
+#include <string.h>
+#include <stdlib.h>
+#include <curses.h>
+#include <stdbool.h>
+
+#define ROW_NUM 20
+#define COL_NUM 54
+#define WIN_MENU_DIFF_ROW 6
+
+void print_in_middle(WINDOW *win, int y, int startx, int width, char *string, chtype color) {
+    int x;
+
+    if(win == NULL)
+        win = stdscr;
+    if(y == 0)
+        getyx(win, y, x);
+    x = startx + (width - strlen(string)) / 2;
+
+    wattron(win, color);
+    mvwprintw(win, y, x, "%s", string);
+    wattroff(win, color);
+    refresh();
+}
+
+void set_item_name (ITEM *item, const char* name)
+{   
+    item->name.str = name;
+}
+
+void refresh_menu(WINDOW *my_win, MENU *menu)
+{
+    set_menu_win(menu, my_win);
+    set_menu_sub(menu, derwin(my_win, ROW_NUM-WIN_MENU_DIFF_ROW, COL_NUM-5, 4, 2));
+    set_menu_mark(menu, "-> ");
+    menu_opts_off(menu, O_SHOWDESC);
+    menu_opts_off(menu, O_ONEVALUE);
+    set_menu_format(menu, ROW_NUM-WIN_MENU_DIFF_ROW, 1); // row, column
+
+    box(my_win, 0, 0);
+    print_in_middle(my_win, 1, 0, 50, "Git Branch Tools", COLOR_PAIR(0));
+    mvwhline(my_win, 2, 1, ACS_HLINE, COL_NUM-2);
+    mvprintw(LINES - 2, 0, "Q/q to exit");
+    refresh();
+
+    post_menu(menu);
+    wrefresh(my_win);
+}
+
+
+char *choice_interactive(size_t branch_count, size_t current_branch_index, char **branch_index, size_t *delete_branch_mark)
+{
+    ITEM **items = NULL, *tmp_item = NULL;
+    int c;
+    size_t choice;
+    MENU *menu;
+    WINDOW *win;
+    initscr();
+    start_color();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+
+    items = calloc(branch_count+1, sizeof(ITEM *));
+    for(size_t i=0; i<branch_count; i++)
+        items[i] = new_item(branch_index[i], branch_index[i]);
+
+    menu = new_menu((ITEM **)items);
+
+    /* newwin(int nlines, int ncols, int begin_y, int begin_x)*/
+    win = newwin(ROW_NUM, COL_NUM, 8, 13);
+    keypad(win, TRUE);
+
+    refresh_menu(win, menu);
+
+    while((c = wgetch(win)) != KEY_F(1))
+    { 
+        switch(c)
+        {
+            case 'j':
+            case 'J':
+            case KEY_DOWN:
+                menu_driver(menu, REQ_DOWN_ITEM);
+                break;
+            case 'k':
+            case 'K':
+            case KEY_UP:
+                menu_driver(menu, REQ_UP_ITEM);
+                break;
+            case 'h':
+            case 'H':
+            case KEY_LEFT:
+                menu_driver(menu, REQ_LEFT_ITEM);
+                break;
+            case 'l':
+            case 'L':
+            case KEY_RIGHT:
+                menu_driver(menu, REQ_RIGHT_ITEM);
+                break;
+            case 'm':
+                menu_driver(menu, REQ_SCR_DPAGE);
+                break;
+            case 'n':
+                menu_driver(menu, REQ_SCR_UPAGE);
+                break;
+            case ' ':
+                menu_driver(menu, REQ_TOGGLE_ITEM);
+                break;
+            case 'o': case 'O': case 10:
+                tmp_item = current_item(menu);
+                choice = item_index(tmp_item);
+                move(LINES - 3, 0);
+                clrtoeol();
+                mvprintw(LINES - 3, 0, "%s", item_name(tmp_item));
+                refresh();
+                goto exit;
+                break;
+
+            case 'r':
+                set_current_item(menu, items[4]); // 可用于跳转
+                break;
+
+            case 'd':
+            case 'D':
+                set_item_name(current_item(menu), "deleted!");
+                set_current_item(menu, current_item(menu));
+                refresh_menu(win, menu);
+                break;
+
+            case 'q':case 'Q': case 67:
+                goto exit;
+        }
+        wrefresh(win);
+    }
+
+exit:
+    unpost_menu(menu);
+    free_menu(menu);
+    for(size_t i=0; i<branch_count; i++)
+        free_item(items[i]);
+    free(items);
+    endwin();
+
+    return branch_index[choice];
+}
+
+bool is_space(int arg)
+{
+    return \
+        arg == ' '  || \
+        arg == '\t' || \
+        arg == '\r' || \
+        arg == '\v' || \
+        arg == '\f' || \
+        arg == '\r';
+}
+
+bool is_newline(int arg)
+{
+    return arg == '\n';
+}
+
+int get_raw_output_from_git_branch(char **input_buf)
+{
+    FILE *fp = popen("git branch", "r");
+    if(fp == NULL)
+    {
+        fprintf(stderr, "popen(\"git branch\") execute failed. line: %d", __LINE__);
+        exit(1);
+    }
+
+#define BASE_READ_SIZE 1024
+
+    char read_buf[BASE_READ_SIZE] = { '\0' };
+    size_t read_size = 0;
+
+    size_t current_raw_size = BASE_READ_SIZE;
+    char *raw_buf = calloc(current_raw_size, sizeof(char));
+    size_t offset = 0;
+
+    /* update the address pointed to by input_buf*/
+    *input_buf = raw_buf;
+
+    /* Read all text from pipleline, and dynamic allocate memory to hold the output text.*/
+    while(true)
+    {
+        /* NOTE: reach the trailing character: '\0' will return read_size = 0*/
+        if( (read_size = fread(read_buf, sizeof(char), sizeof(read_buf), fp)) < 0 )
+        {
+            fprintf(stderr, "fread data from popen file pointer failed. line: %d", __LINE__);
+            exit(1);
+        }
+
+        /* printf("read_size: %ld sizeof(read_buf): %ld read_buf: %s<\n", read_size, sizeof(read_buf), read_buf); */
+
+        /* Read the ending character: '\0';*/
+        if(read_size == 0)
+        {
+            /* printf("last time reach the trailing character: '\0'.\n"); */
+            raw_buf[offset] = '\0';
+            break;
+        }
+
+        /* No more text found from pipeline*/
+        if(read_size < sizeof(read_buf))
+        {
+            /* trailing character will not be read into read_buf, we need to add it ourselves*/
+            read_buf[read_size] = '\0';
+
+            strcpy(raw_buf+offset, read_buf);
+            /* printf("No more text found from pipeline, %d %d\n", read_size < sizeof(read_buf), read_buf[sizeof(read_buf)-1] == '\0'); */
+            break;
+        }
+
+        /* raw_buff is no more space to hold the pipeline text, alloc large memory*/
+        if(offset+read_size >= current_raw_size)
+        {
+            current_raw_size *= 2;
+            *input_buf = raw_buf = realloc(raw_buf, current_raw_size);
+            /* printf("raw_buff is no more space to hold the pipeline text, alloc large memory, realloc size: %zu\n", current_raw_size); */
+        }
+
+        strcpy(raw_buf+offset, read_buf);
+        offset += read_size;
+    }
+
+    /* printf("git branch output:\n<%s>\n", raw_buf); */
+
+    return 0;
+}
+
+int parse_raw_output_of_git_branch(char *raw_buf, size_t *current_branch_index, char ***input_buf, char ***dup_input_buf)
+{
+    size_t base_branch_size = 6;
+    size_t branch_count = 0;
+    size_t max_branch_size = base_branch_size;
+    char **branch_index = calloc(base_branch_size, sizeof(char*));
+
+    /* update the address pointed to by input_buf*/
+    *input_buf = branch_index;
+
+    char *char_ptr = raw_buf;
+    char *branch_name_start = NULL;
+    bool branch_not_found = true;
+    while( *char_ptr )
+    {
+        if(is_space(*char_ptr))
+        {
+            char_ptr++;
+            continue;
+        }
+        /* else if(branch_not_found && *char_ptr != '*') */
+        else if(branch_not_found)
+        {
+            branch_name_start = char_ptr;
+            branch_not_found = false;
+            if(*branch_name_start == '*')
+                *current_branch_index = branch_count;
+        }
+
+        /* NOTE: did not handle the not newline trailing text*/
+        if(is_newline(*char_ptr))
+        {
+            *char_ptr = '\0';
+            branch_index[branch_count] = calloc(sizeof(char), strlen(branch_name_start)+1);
+            strcpy(branch_index[branch_count++], branch_name_start);
+            /* printf("found branch: %s\n", branch_index[branch_count-1]); */
+            branch_not_found = true;
+
+            if(branch_count == max_branch_size)
+            {
+                max_branch_size *= 2;
+                *input_buf = branch_index = realloc(branch_index, max_branch_size*sizeof(char*));
+                /* printf("branch count > max branch size, realloc branch size: %ld\n", max_branch_size); */
+            }
+        }
+
+        char_ptr++;
+    }
+
+    /* printf("branch count: %ld \n", branch_count); */
+
+    return branch_count;
+}
+
+int get_all_branch_name(size_t *branch_count, size_t *current_branch_index, char ***branch_index, char ***branch_index_hint)
+{
+    char *raw_buf = NULL;
+    get_raw_output_from_git_branch(&raw_buf);
+
+    *branch_count = parse_raw_output_of_git_branch(raw_buf, current_branch_index, branch_index, branch_index_hint);
+
+    free(raw_buf);
+
+    return 0;
+}
+
+void switch_branch(char *choice_branch_name)
+{
+    if(!choice_branch_name)
+        return;
+
+    char *branch_name_start = choice_branch_name;
+    if(choice_branch_name[0] == '*')
+    {
+        char *char_ptr = choice_branch_name + 1;
+        while(*char_ptr)
+        {
+            if(is_space(*char_ptr) )
+            {
+                char_ptr++;
+                continue;
+            }
+
+            branch_name_start = char_ptr;
+            break;
+        }
+    }
+
+    char *git_command = "git checkout ";
+    char *command = calloc(sizeof(char), strlen(branch_name_start)+strlen(git_command)+1);
+
+    strcat(command, git_command);
+    strcat(command, branch_name_start);
+
+    system(command);
+
+    free(command);
+}
+
+int main()
+{
+    char **branch_index = NULL;
+    char **branch_index_hint = NULL;
+    size_t branch_count = 0;
+    size_t current_branch_index = 0;
+    get_all_branch_name(&branch_count, &current_branch_index, &branch_index, &branch_index_hint);
+
+    size_t *delete_branch_mark = calloc(branch_count, sizeof(size_t));
+    char *choice_branch_name = choice_interactive(branch_count, current_branch_index, branch_index, delete_branch_mark);
+
+    switch_branch(choice_branch_name);
+
+    /* clean resources*/
+    for(size_t i=0; i<branch_count; i++)
+        if(branch_index[i])
+            free(branch_index[i]);
+
+    if(branch_index)
+        free(branch_index);
+
+    if(delete_branch_mark)
+        free(delete_branch_mark);
+
+    return 0;
+}
+
