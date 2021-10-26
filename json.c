54c0a022c80037b91ceeffc8659e2c87f55d5571
poemdistance <poemdistance@gmail.com>
2021-10-27 00:24:53 +0800

    bak json

--- json.c
+++ json.c
@@ -1,6 +1,42 @@
 #include "json.h"
 
+
+typedef struct json_obj
+{
+    int key_type;
+    int value_type;
+    char *key;
+    void *normal_value;
+    struct json_obj **obj_value;
+
+} JsonObj;
+
+typedef JsonObj* JsonObjPtr;
+
+JsonObjPtr *init_json_obj()
+{
+    JsonObjPtr json_obj = calloc(sizeof(JsonObj), 1);
+    return json_obj;
+}
+
+void load_from_string(char *json)
+{
+    if(!json)
+        return NULL;
+
+    if(*json == '\0')
+        return json;
+
+    char *p = json;
+    while(*p)
+    {
+
+    }
+}
+
 int main(int argc, char **argv)
 {
+    char *json_str = "{\"a\": 1, \"b\": [1, 2, 3]}";
+    load_from_string(json_str);
     return 0;
 }
